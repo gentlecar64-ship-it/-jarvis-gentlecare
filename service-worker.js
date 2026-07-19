047921258609a03a9c908ccb3414519309feb401
gentlecar64-ship-it <gentlecar64@gmail.com>
2026-07-19 04:58:07 +0200

    feat: publish workshop orders in PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='jarvis-gentlecare-v23';
-const CORE=['./','./index.html','./planning.html','./employe.html','./admin.html','./gestion.html','./clients.html','./stock.html','./devis.html','./gcos-comms.js','./jarvis-core.js','./icon.svg','./manifest.webmanifest','./storage.js','./install.js','./boot.js','./jarvis-responsive.css','./atelier-responsive.css'];
+const CACHE='jarvis-gentlecare-v24';
+const CORE=['./','./index.html','./planning.html','./employe.html','./admin.html','./gestion.html','./clients.html','./stock.html','./devis.html','./ordres.html','./gcos-comms.js','./jarvis-core.js','./icon.svg','./manifest.webmanifest','./storage.js','./install.js','./boot.js','./jarvis-responsive.css','./atelier-responsive.css'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())});
 self.addEventListener('fetch',event=>{
@@ -11,10 +11,10 @@ self.addEventListener('fetch',event=>{
       let finalResponse=response;
       if(url.pathname.endsWith('/employe.html')){
         const html=await response.text();
-        finalResponse=new Response(html.replace('</head>','<link rel="stylesheet" href="atelier-responsive.css?v=23"></head>'),{status:response.status,statusText:response.statusText,headers:{'Content-Type':'text/html; charset=utf-8'}});
+        finalResponse=new Response(html.replace('</head>','<link rel="stylesheet" href="atelier-responsive.css?v=24"></head>'),{status:response.status,statusText:response.statusText,headers:{'Content-Type':'text/html; charset=utf-8'}});
       }else if(url.pathname.endsWith('/admin.html')){
         let html=await response.text();
-        html=html.replace("['▤','Devis','Offres et validations','']","['▤','Devis','Offres et validations','devis.html']").replace("['◫','Commandes','Achats et livraisons','']","['◫','Ordres de travail','Suivi atelier','devis.html']").replace("['🏭','Fournisseurs','Contacts techniques','']","['🏭','Fournisseurs','Contacts techniques','stock.html?tab=suppliers']").replace("['❄','Stocks','Glace et consommables','']","['❄','Stocks','Glace et consommables','stock.html']");
+        html=html.replace("['▤','Devis','Offres et validations','']","['▤','Devis','Offres et validations','devis.html']").replace("['◫','Commandes','Achats et livraisons','']","['◫','Ordres de travail','Suivi atelier','ordres.html']").replace("['🏭','Fournisseurs','Contacts techniques','']","['🏭','Fournisseurs','Contacts techniques','stock.html?tab=suppliers']").replace("['❄','Stocks','Glace et consommables','']","['❄','Stocks','Glace et consommables','stock.html']").replace("['🛠','Interventions','Rapports et qualité','employe.html']","['🛠','Interventions','Rapports et qualité','ordres.html']");
         finalResponse=new Response(html,{status:response.status,statusText:response.statusText,headers:{'Content-Type':'text/html; charset=utf-8'}});
       }
       const copy=finalResponse.clone();
